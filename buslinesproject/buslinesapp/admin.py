e1e18940dac38c8c8928ec68629168fe82737a63
ThangSaDekk <thang111103@gmail.com>
2024-06-12 20:54:13 +0700

    C13

--- buslinesproject/buslinesapp/admin.py
+++ buslinesproject/buslinesapp/admin.py
@@ -2,9 +2,37 @@ from django.contrib import admin
 from .models import Account, BusRoute, BusLine, BusInfor, Seat, Delivery, Ticket, Review, Bill
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
+from django.urls import path
+from django.db.models import Count, Sum
+from django.template.response import TemplateResponse
 
 
 # Register your models here.
+class MyBusLineManagerAdminSite(admin.AdminSite):
+    site_header = 'BusLineManagerOnline'
+
+    def get_urls(self):
+        return [path('businfors-stats/', self.stats_view)] + super().get_urls()
+
+    def stats_view(self, request):
+        busroute_stats = BusInfor.objects.annotate(c=Count('busroute__id')).values('id', 'name', 'c').filter(
+            active=True)
+        busline_stats = BusInfor.objects.annotate(c=Count('busroute__busline__id')).values('id', 'name', 'c').filter(
+            active=True)
+        bill_stats = BusInfor.objects.annotate(
+            c=Sum('busroute__busline__seat__ticket__bill__total', distinct=True ,default=0)).values('id',
+                                                                                         'name',
+                                                                                         'c').filter(
+            active=True)
+        context = {
+            "busroute_stats": busroute_stats,
+            "busline_stats": busline_stats,
+            "bill_stats": bill_stats,
+        }
+        return TemplateResponse(request, 'admin/stats.html', context=context)
+
+
+admin_site = MyBusLineManagerAdminSite(name='BusLineManager')
 
 
 class DeliveryForm(forms.ModelForm):
@@ -50,15 +78,15 @@ class MyTicketAdmin(admin.ModelAdmin):
 
 
 class MyReviewAdmin(admin.ModelAdmin):
-    list_display = ['id','code', 'rating', 'review_time', 'comment']
+    list_display = ['id', 'code', 'rating', 'review_time', 'comment']
 
 
-admin.site.register(Account, MyAccountAdmin)
-admin.site.register(BusInfor, MyBusInforAdmin)
-admin.site.register(BusRoute, MyBusRouteAdmin)
-admin.site.register(BusLine, MyBusLineAdmin)
-admin.site.register(Seat, MySeatAdmin)
-admin.site.register(Bill, MyBillAdmin)
-admin.site.register(Ticket, MyTicketAdmin)
-admin.site.register(Delivery)
-admin.site.register(Review, MyReviewAdmin)
+admin_site.register(Account, MyAccountAdmin)
+admin_site.register(BusInfor, MyBusInforAdmin)
+admin_site.register(BusRoute, MyBusRouteAdmin)
+admin_site.register(BusLine, MyBusLineAdmin)
+admin_site.register(Seat, MySeatAdmin)
+admin_site.register(Bill, MyBillAdmin)
+admin_site.register(Ticket, MyTicketAdmin)
+admin_site.register(Delivery)
+admin_site.register(Review, MyReviewAdmin)
