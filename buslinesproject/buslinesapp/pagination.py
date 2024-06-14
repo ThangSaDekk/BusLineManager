adfe5c86e0dfc7d741917591564f75d72e8208a9
ThangSaDekk <thang111103@gmail.com>
2024-06-14 18:22:47 +0700

    C15

--- buslinesproject/buslinesapp/pagination.py
+++ buslinesproject/buslinesapp/pagination.py
@@ -2,7 +2,7 @@ from rest_framework import pagination
 
 
 class BusInforPaginator(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 5
 
 
 class BusRoutePaginator(pagination.PageNumberPagination):
